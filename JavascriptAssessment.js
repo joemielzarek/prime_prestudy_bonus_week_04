e5b1820c7f0f3228f3eb1df63a94e71e460d049b
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-04-17 11:42:15 -0500

    first commit

--- JavascriptAssessment.js
+++ JavascriptAssessment.js
@@ -3,32 +3,32 @@ var theNumber = 1;
 var michelangelo = function(value){
     value = (value * 2);
     return value;
-}
+};
  
 var donatello = function(value, anotherValue){
     value = anotherValue + value;
     anotherValue = value * anotherValue;
     return anotherValue;
-}
+};
  
 var leonardo = function(value){
     var anotherValue = value;
     value *= 4;
     theNumber = value / 2;
     return (value + anotherValue);
-}
+};
  
 var raphael = function(value, anotherValue, yetAnotherValue){
     value = value * anotherValue + yetAnotherValue;
     return yetAnotherValue;
-}
+};
 
 //Newly created function
 var splinter = function(number1, number2) {
 	var z = number1 * number2;
 	var pizza = [number1, number2, z];
 	return pizza;
-}
+};
 
 //In your email, tell me what the answer to this expression would be:
 michelangelo(theNumber) + leonardo(theNumber); //This expression equates to 7
